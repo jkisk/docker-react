7f8898db3672a4ac28e0d8ea537caa3e4be6390c
Jamie Kinkead <jkinkead@gmail.com>
2019-04-29 15:08:39 -0700

    Change text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          BlI was there!!!
+          Oh boy was I here!!!
         </p>
         <a
           className="App-link"
